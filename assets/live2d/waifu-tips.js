560a3f7e0318b6d553ba6276170e2d6a652b0a55
Lucas04-nhr <lucas04@foxmail.com>
2024-09-06 09:24:05 +0800

    Update home page URL in live2d settings

--- assets/live2d/waifu-tips.js
+++ assets/live2d/waifu-tips.js
@@ -66,7 +66,7 @@ live2d_settings['waifuDraggableRevert'] = false;         // 松开鼠标还原�
 // 其他杂项设置
 live2d_settings['l2dVersion']           = '1.4.2';        // 当前版本
 live2d_settings['l2dVerDate']           = '2024.05.25'; // 版本更新日期
-live2d_settings['homePageUrl']          = 'auto';       // 主页地址，可选 'auto'(自动), '{URL 网址}'
+live2d_settings['homePageUrl']          = 'https://blog.lucas04.xyz/';       // 主页地址，可选 'auto'(自动), '{URL 网址}'
 live2d_settings['aboutPageUrl']         = 'https://blog.lucas04.xyz/about';
 live2d_settings['screenshotCaptureName']= 'live2d.png'; // 看板娘截图文件名，例如 'live2d.png'
 
