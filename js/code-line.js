5647b525aa25aa2665e8382bd10b31cd27572909
Lucas04-nhr <lucas04@foxmail.com>
2024-09-30 11:58:46 +0800

    Refactor code-line.js to improve code line counting accuracy

--- js/code-line.js
+++ js/code-line.js
@@ -2,18 +2,23 @@
 $(document).ready(function () {
   function get_count(code_text) { //计算代码行数
     const lines = code_text.split('\n');
-    return lines.length;
+    return lines.length; // 包括空行
   }
 
   function create_ul(code_text) { //根据行数生成ul标签
-    const code_count = get_count(code_text);
+    const lines = code_text.split('\n');
     const res_ul = $('<ul class="code-number"></ul>');
-    for (let i = 1; i <= code_count; i++) {
-      const li = $(`<li>${i}</li>`);
+    
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      // 检查每行是否为空，空行也计入行号
+      const li = $(`<li>${i + 1} ${line ? '' : ' '}</li>`);
       res_ul.append(li);
     }
+    
     return res_ul;
   }
+
   const code_pre = $(".highlighter-rouge pre.highlight"); //代码框
   code_pre.each(function (index, dom) {
     if ($(dom).parents(".language-plaintext")[0]) { //如果是普通文本框
