be4a9ffd47adfe13d78f5ddd51d59a80673c3c94
Lucas04-nhr <lucas04@foxmail.com>
2024-09-30 12:08:22 +0800

    Refactor code-line.js to improve code line counting accuracy and update code-block.html to include code line numbering styles

--- js/code-line.js
+++ js/code-line.js
@@ -1,31 +1,27 @@
-// 代码框显示行数
+// 代码行数显示
 $(document).ready(function () {
-  function get_count(code_text) { // 计算代码行数
-    const lines = code_text.split('\n');
-    return lines.length; // 包括空行
+  function get_count(code_text) { //计算代码行数
+    const regex = /\n/g;
+    const match = code_text.match(regex);
+    const count = match ? match.length : 0;
+    return count;
   }
-
-  function create_ul(code_text) { // 根据行数生成ul标签
-    const lines = code_text.split('\n');
+  function create_ul(code_text) { //根据行数生成ul标签
+    const code_count = get_count(code_text);
     const res_ul = $('<ul class="code-number"></ul>');
-
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i];
-      // 生成行号，空行仍然显示行号
-      const li = $(`<li>${i + 1}</li>`);
+    for (let i = 1; i <= code_count; i++) {
+      const li = $(`<li>${i}</li>`);
       res_ul.append(li);
     }
-
     return res_ul;
   }
-
-  const code_pre = $(".highlighter-rouge pre.highlight"); // 代码框
+  const code_pre = $(".highlighter-rouge pre.highlight"); //代码框
   code_pre.each(function (index, dom) {
-    if ($(dom).parents(".language-plaintext")[0]) { // 如果是普通文本框
+    if ($(dom).parents(".language-plaintext")[0]) { //如果是普通文本框
       $(dom).css("cssText", "padding-left: 16px !important;");
-      return; // 不显示行数
+      return; //不显示行数
     }
-    const code_text = $(dom).children('code').text(); // 文本内容
-    $(dom).append(create_ul(code_text)); // 循环添加
+    const code_text = $(dom).children('code').text(); //文本内容
+    $(dom).append(create_ul(code_text)); //循环添加
   });
 });
